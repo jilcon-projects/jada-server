59a8b59c441bbbc896ec43beac704e89c49ecb97
Uwakmfon Akpan <astongemmy@gmail.com>
2025-04-22 19:37:56 +0100

    Updated Dockerfile file.

--- scripts/docker/push.sh
+++ scripts/docker/push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source .env
+cat .env
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< APP NAME IS >>>>>>>>>>>>>>>>>>>>>>>>>>> ${APP_NAME}"
 docker tag $APP_NAME $DOCKER_USERNAME/$APP_NAME:latest
 docker push $DOCKER_USERNAME/$APP_NAME:latest
