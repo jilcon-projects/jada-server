49ab4cd3d0a3395fcfe6a4f7944cc4aae765d50a
Uwakmfon Akpan <astongemmy@gmail.com>
2025-04-24 15:22:59 +0100

    Updated deployment file.

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -3,7 +3,6 @@
 docker compose pull
 
 # docker compose build jada-server
-./scripts/install.sh
+# ./scripts/install.sh
 
-# docker-compose build --entrypoint=./scripts/install.sh jada-server --remove-orphans
-docker compose build jada-server
+docker compose run --entrypoint=./scripts/install.sh jada-server --remove-orphans
