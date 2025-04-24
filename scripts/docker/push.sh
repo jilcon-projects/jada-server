313e165b7996de2ab2ff3704e5eb9aef384d0cce
Uwakmfon Akpan <astongemmy@gmail.com>
2025-04-24 15:33:27 +0100

    Updated deployment file.

--- scripts/docker/push.sh
+++ scripts/docker/push.sh
@@ -2,6 +2,7 @@
 
 source .env
 
+docker tag "$REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$SERVICE:$SHA"
 # docker tag $APP_NAME $DOCKER_USERNAME/$APP_NAME:latest
 docker tag jada astongemmy/jada:latest
 docker push astongemmy/jada:latest
